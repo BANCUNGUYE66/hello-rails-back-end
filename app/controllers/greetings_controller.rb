fc967fc1cb3b7440677a76ce3932f98ec0337eb1
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-09-26 19:45:23 +0200

    final product

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,7 @@
+class GreetingsController < ApplicationController
+    def random_greeting
+      greeting = Message.order('RANDOM()').first
+      render json: { greeting: greeting.content }
+    end
+  end
+  
